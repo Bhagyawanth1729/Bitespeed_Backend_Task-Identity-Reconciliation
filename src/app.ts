0ed02ac0e52d688d671db929fcb6214f364e0573
Bhagyawanth <121920324+Bhagyawanth1729@users.noreply.github.com>
2026-02-28 15:15:38 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ import identifyRoute from "./routes/identify";
 const app = express();
 
 app.use(express.json());
-app.use("/", identifyRoute);
+
+app.use("/identify", identifyRoute);
+
+app.get("/", (req, res) => {
+  res.send("Bitespeed Identity Reconciliation API is running 🚀");
+});
 
 const PORT = process.env.PORT || 3000;
 
